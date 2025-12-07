a98eb152963ae9fa977dafef0321c2eafa2a1567
shahryar-rwotech <m.shahryar@rwgroup.om>
2025-12-07 22:34:30 +0400

    last modified

--- app.py
+++ app.py
@@ -2,13 +2,8 @@ def addition(a, b, c):
     return a+b+c
 
 
-<<<<<<< HEAD
 def subtraction(a, b, c):
     return a-b-c
-=======
-def subtraction(a, b, e):
-    return a-b-e
->>>>>>> 2e065b897f8c422967880a087e19f03267c3f4eb
 
 
 def division(a, b):
